e74c8c06473d10f1a3ef7ea91500e11b068ae6a4
Yusuf <52000549+koprendo@users.noreply.github.com>
2022-04-15 12:26:29 +0300

    Create delete_table.sql

---
+++ delete_table.sql
@@ -0,0 +1,6 @@
+-- in order to delete table, use DROP TABLE statement:
+DROP TABLE test;
+
+-- We can use it with IF EXISTS keywords:
+DROP TABLE IF EXISTS test;
+-- So, it doesn't return an error if there is not a table called test
