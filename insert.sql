3c19016a46ed4dcd2f2250e8b2b74f94c0714a9c
Yusuf <52000549+koprendo@users.noreply.github.com>
2022-04-15 12:42:36 +0300

    Update insert.sql
    These are added:
    - "default values" keywords
    - INSERT statement with SELECT statement

--- insert.sql
+++ insert.sql
@@ -9,3 +9,11 @@ INSERT INTO customer (name, city, state)
   VALUES ('Jimi Hendrix', 'Renton', 'WA');
 -- Now, we have null values (which means no data) in those columns that we were
 -- not specified.   
+
+-- We can add a row with no data in it by using DEFAULT VALUES keywords:
+INSERT INTO test DEFAULT VALUES;
+
+-- We can use the results of a SELECT statemnt to add rows to a table:
+INSERT INTO test SELECT id, name, description FROM item;
+-- We can also filter the results:
+INSERT INTO test SELECT id, name, description FROM item WHERE id = 2;
