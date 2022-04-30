a421942f992d44a4f05bf97602512b7d255e0260
Yusuf <52000549+koprendo@users.noreply.github.com>
2022-04-30 16:33:20 +0300

    Create select_distinct.sql

---
+++ select_distinct.sql
@@ -0,0 +1,5 @@
+-- in order to list all the different values, use SELECT DISTINCT:
+SELECT DISTINCT Continent FROM Country;
+
+-- This will give the distinct rows where a and b, together, are different:
+SELECT DISTINCT a, b FROM test;
